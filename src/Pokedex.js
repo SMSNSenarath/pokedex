e9b5c5a716a7c4df60ed3229e52bdd4bfac485a4
Shakila Nishadini <smsnshakilaa372@gmail.com>
2022-10-24 20:07:24 +0530

    Added Pokegame component

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -22,7 +22,6 @@ class Pokedex extends Component{
             <div className='Pokedex-cards'>
             {this.props.pokemon.map((p)=>(
                 <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience}/>
-                
             ))}
             </div>
         </div>
