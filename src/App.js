6a9d37cf3573ec0207770210c7a52212a64b3e0e
Shakila Nishadini <smsnshakilaa372@gmail.com>
2022-10-24 18:29:19 +0530

    Setup Pokedex component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, {Component} from 'react';
-import Pokecard from './Pokecard';
+import Pokedex from './Pokedex';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App"> 
-    <Pokecard/>
-    </div>
-
+class App extends Component() {
+  render(){
+    return (
+      <div className="App"> 
+      <Pokedex/>
+      </div>
   
-  );
+    
+    );
+  }
+
 }
 
 export default App;
