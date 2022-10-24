682d26bca5d9ebda44650dfd8920380694c22098
Shakila Nishadini <smsnshakilaa372@gmail.com>
2022-10-24 18:55:14 +0530

    Added stylings

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+import './Pokecard.css';
 
 class Pokecard extends Component {
     render() {
         let imgSrc = `${POKE_API}${this.props.id}.png`
         return (
             <div className='Pokecard'>
-                <h1>{this.props.name}</h1>
+                <h1 className='Pokecard-title'>{this.props.name}</h1>
                 <img src={imgSrc} alt={this.props.name}/>
-                <div>Type: {this.props.type}</div>
-                <div>EXP: {this.props.exp}</div>
+                <div Pokecard-data>Type: {this.props.type}</div>
+                <div Pokecard-data>EXP: {this.props.exp}</div>
             </div>
         )
     }
